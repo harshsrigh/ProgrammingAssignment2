13444e01d2375a19b9240d63212d74a92979e0fa
harshsrigh <harshsrigh@gmail.com>
2019-02-19 22:49:20 +0530

    Updating script as per the requirment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Used to calculate the Inverse of the matrix
+## To save the time and computation power, Program saves the result
+## in the cache memory using cacheSolve function
 
-## Write a short comment describing this function
+## This function returns a makes a magic "matrix"  
+## which used by CacheSolve for saving the Inverse of 
+## input matrix in the Memory(This function use lexical scoping)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), then 
+## the cachesolve retrieves the inverse from the cache
 
 cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
